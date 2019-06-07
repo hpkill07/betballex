a28bd2f8dba6d741e20066330d0ab07d5c9f660b
hpkill07 <41097860+hpkill07@users.noreply.github.com>
2019-06-07 09:39:41 +0700

    Update getlink.py

--- getlink.py
+++ getlink.py
@@ -1,43 +1,51 @@
 import requests
 from bs4 import BeautifulSoup
 import mysql.connector
+import schedule
+import time
 
 
+def job():
+ print("I'm working...")
 
-url = "https://www.betexplorer.com/next/soccer/"
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
-tb = soup.find('table', class_='table-main js-nrbanner-t')
+ url = "https://www.betexplorer.com/next/soccer/"
+ page = requests.get(url)
+ soup = BeautifulSoup(page.content, 'html.parser')
+ tb = soup.find('table', class_='table-main js-nrbanner-t')
 
-for link in tb.find_all('td', class_='table-main__tt'):
-    name = link.findAll('a')
-    x = str(name)
-    x1 = x.replace('[<a href="','')
-    x2 = x1.replace('"><span>',',')
-    x3 = x2.replace(']','')
-    x4 = x3.replace('[<a data-live-cell="matchlink" href="','')
-    x5 = x4.split(',')
+ for link in tb.find_all('td', class_='table-main__tt'):
+        name = link.findAll('a')
+        x = str(name)
+        x1 = x.replace('[<a href="','')
+        x2 = x1.replace('"><span>',',')
+        x3 = x2.replace(']','')
+        x4 = x3.replace('[<a data-live-cell="matchlink" href="','')
+        x5 = x4.split(',')
 
-    x6 = 'https://www.betexplorer.com'+x5[0]
-    print(x6)
-    print(type(x6))
 
-    mydb = mysql.connector.connect(
-        host="202.129.206.136",
-        user="darunph3_darunph3",
-        passwd="Por19030703",
-        database="darunph3_bet"
-    )
+        x6 = 'https://www.betexplorer.com'+x5[0]
+        print(x6)
 
-    mycursor = mydb.cursor()
-    sql = "INSERT INTO betList (betListLink, status) VALUES (%s, %s)"
+        mydb = mysql.connector.connect(
+            host="202.129.206.136",
+            user="darunph3_darunph3",
+            passwd="Por19030703",
+            database="darunph3_bet"
+        )
 
-    val = (x6,"0")
-    mycursor.execute(sql, val)
+        mycursor = mydb.cursor()
+        sql = "INSERT IGNORE INTO betList (betListLink, fcName) VALUES (%s, %s)"
 
-    mydb.commit()
+        val = (x6, "0")
+        mycursor.execute(sql, val)
 
-    print(mycursor.rowcount, "record inserted.")
+        mydb.commit()
+
+        print(mycursor.rowcount, "record inserted.")
+schedule.every().day.at("09:40").do(job)
+while True:
+ schedule.run_pending() # รันตารางเวลา
+ time.sleep(1)
 
 
 
